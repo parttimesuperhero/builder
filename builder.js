93d19d051df63adbdee2279e6a5c738c33a56e5d
Matt Hollis <hollimat@IOM5006630.local>
2017-04-21 15:46:56 +0100

    Adding default gulp tast

--- builder.js
+++ builder.js
@@ -5,3 +5,5 @@ require('./tasks/scripts')(gulp);
 require('./tasks/styles')(gulp);
 require('./tasks/views')(gulp);
 require('./tasks/webserver')(gulp);
+
+gulp.task('default', ['styles', 'js', 'views']);
