d940d2ad910f21f5771a84337a30a845a065ea93
Matt Hollis <hollimat@IOM5006630.local>
2017-04-28 11:02:25 +0100

    Adding images to webserver

--- tasks/webserver.js
+++ tasks/webserver.js
@@ -7,7 +7,7 @@ const directory = fs.realpathSync('./gulpfile.js').replace('/gulpfile.js', '');
 
 module.exports = function(gulp){
   // Fires up the localhost test environment
-  gulp.task('webserver', ['styles:watch', 'js:watch', 'views:watch'], () => {
+  gulp.task('webserver', ['styles:watch', 'js:watch', 'views:watch', 'images:watch'], () => {
     gulp.src(`${directory}/dist`)
       .pipe(webserver({
         livereload: true,
