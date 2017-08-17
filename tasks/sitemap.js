ef4e1dadbc7d2ab6a480eb92f73457bc2c1ee9d5
Matt Hollis <hollimat@IOM5006630.local>
2017-08-17 09:29:10 +0100

    only remove .html extensions

--- tasks/sitemap.js
+++ tasks/sitemap.js
@@ -21,7 +21,7 @@ module.exports = function(gulp){
     .pipe(sitemap({
       siteUrl: 'http://www.starsgroup.com',
       getLoc: function(siteUrl, loc, entry) {
-          return loc.substr(0, loc.lastIndexOf('.')) || loc; // Removes the file extension
+          return loc.substr(0, loc.lastIndexOf('.html')) || loc; // Removes the file extension
       }
     }))
     .pipe(gulp.dest(config.dest));
